68c336477db607dfad4568fe59220ddfdc7871f8
Vladimir <kaminskiy2704@gmail.com>
2022-01-09 15:10:33 +0200

    add files_test

---
+++ files_test.py
@@ -0,0 +1,13 @@
+import os
+
+def get_names(path):
+    for root, directories, files in os.walk(path, topdown=True):
+        for name in files:
+            print(os.path.join(root, name))
+        for name in directories:
+            print(os.path.join(root, name))
+
+
+if __name__ == "__main__":
+    path = '/Users/kaminsk1y/projects/hillel_2021app'
+    get_names(path)
