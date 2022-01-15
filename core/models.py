4b9d6a8a928b97a9c0ac3785eeb7e17810946047
kam1nskiy <kaminskiy2704@gmail.com>
2022-01-15 13:24:41 +0200

    update models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+from operator import mod
+from turtle import title
 from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null = False)
+    content = models.TextField()
+    image = models.CharField(max_length=255, null = False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,on_delete=models.SET_NULL)
+
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
 
 # Create your models here.
