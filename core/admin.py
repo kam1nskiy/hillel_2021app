6c6b8536d30895582fb217468eec816840cddb24
kam1nskiy <kaminskiy2704@gmail.com>
2022-01-19 20:14:17 +0200

    add Likes to Post hw15

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+# from .models import Like, Post
+from core import models
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+
+# @admin.register(Like)
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+
+class TabularInlineLike(admin.TabularInline):
+    model=models.Like
+
+class PostAdmin(admin.ModelAdmin):
+    inlines=[TabularInlineLike]
+    list_display = ("title", "user")
+
+admin.site.register(models.Post, PostAdmin)
+
+
+
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,17 +1,57 @@
+from email.policy import default
 from operator import mod
 from turtle import title
 from django.db import models
 from django.conf import settings
 
-class Post(models.Model):
-    title = models.CharField(max_length=100, null = False)
-    content = models.TextField()
-    image = models.CharField(max_length=255, null = False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,on_delete=models.SET_NULL)
+# class Post(models.Model):
+#     title = models.CharField(max_length=100, null = False, blank=False)
+#     content = models.TextField(null = False, blank=False)
+#     image = models.CharField(max_length=255, null = False)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=False, on_delete=models.SET_NULL)
 
 
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-    
+#     created_at = models.DateTimeField(auto_now_add = True)
+#     updated_at = models.DateTimeField(auto_now = True)
 
-# Create your models here.
+#     def __str__(self) -> str:
+#         return f"{self.title}"
+
+
+
+
+# class Like(models.Model):
+#     post = models.ForeignKey("Post", null = False, blank = False, on_delete = models.CASCADE)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, null = False, blank = False, on_delete = models.CASCADE)
+#     status = models.BooleanField(null = True, blank = True)
+
+
+#     created_at = models.DateTimeField(auto_now_add = True)
+#     updated_at = models.DateTimeField(auto_now = True)
+#  Create your models here.
+
+
+class TimeStampMixin(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Post(TimeStampMixin):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=False, on_delete=models.SET_NULL)
+
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+
+class Like(TimeStampMixin):
+    post = models.ForeignKey("Post", null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, blank=False, on_delete=models.CASCADE)
+    status = models.BooleanField(null=True, blank=True)
